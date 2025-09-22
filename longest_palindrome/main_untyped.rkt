db3d4df9aea625e3afebd48f526fd55f3c9ddd48
Yiyun Liu <yliu8e5f7@protonmail.com>
2025-09-22 15:58:47 -0400

    Add faulty implementation of longest palindrome

---
+++ longest_palindrome/main_untyped.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(define (longest-palindrome s)
+  (if (eqv? (string-length s) 0)
+      s
+      (let ([longest-that-ends-at (make-vector (string-length s) 1)])
+        (for ([idx (in-range 1 (string-length s))])
+          (let ([ch (string-ref s idx)]
+                [len (vector-ref longest-that-ends-at (sub1 idx))])
+            (let ([idx0 (- idx len 1)])
+              (when (eqv? ch (string-ref s (sub1 idx)))
+                (vector-set! longest-that-ends-at idx 2))
+              (when (and (natural? idx0) (eqv? ch (string-ref s idx0)))
+                (vector-set! longest-that-ends-at idx (+ len 2))))))
+        (let-values
+            ([(len idx)
+              (for/fold
+            ([acc 0]
+             [accidx -1])
+            ([x longest-that-ends-at]
+             [idx (in-naturals)])
+                (if (<= x acc)
+                    (values acc accidx)
+                    (values x idx)))])
+          (substring s (- (add1 idx) len) (add1 idx))))))
