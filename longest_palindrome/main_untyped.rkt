bcf932c804344fa27860a520b7c240e1df5a55a4
Yiyun Liu <liuyiyun@seas.upenn.edu>
2025-09-22 23:24:40 -0400

    Add the correct solutions of longest palindrome

--- longest_palindrome/main_untyped.rkt
+++ longest_palindrome/main_untyped.rkt
@@ -1,28 +1,31 @@
 #lang racket
 
+(define (expand-palindrome l r s acc)
+  (if (and (>= l 0) (< r (string-length s)) (eqv? (string-ref s l) (string-ref s r)))
+      (expand-palindrome (sub1 l) (add1 r) s (+ acc 1))
+      acc))
+
 (define (longest-palindrome s)
-  (if (eqv? (string-length s) 0)
+  (if (<= (string-length s) 1)
       s
-      (let ([longest-that-ends-at (make-vector (string-length s) 1)]
-            [count 1])
-        (for ([idx (in-range 1 (string-length s))])
-          (let ([ch (string-ref s idx)]
-                [len (vector-ref longest-that-ends-at (sub1 idx))])
-            (let ([idx0 (- idx len 1)])
-              (set! count (if (eqv? ch (string-ref s (sub1 idx)))
-                              (add1 count)
-                              1))
-              (vector-set! longest-that-ends-at idx count)
-              (when (and (natural? idx0) (eqv? ch (string-ref s idx0)))
-                (vector-set! longest-that-ends-at idx (+ len 2))))))
-        (let-values
-            ([(len idx)
-              (for/fold
-            ([acc  0]
-             [accidx -1])
-            ([x  longest-that-ends-at]
-             [idx (in-naturals)])
-                (if (<= x acc)
-                    (values acc accidx)
-                    (values x idx)))])
-          (substring s (- (add1 idx) len) (add1 idx))))))
+      (let-values ([(omaxidx omaxlen)
+                    (for/fold
+      ([maxidx -1]
+       [maxlen -1])
+      ([idx (in-range 0 (string-length s))])
+                      (let ([newlen (expand-palindrome (sub1 idx) (add1 idx) s 0)])
+                        (if (< maxlen newlen)
+                            (values idx newlen)
+                            (values maxidx maxlen))))]
+                   [(emaxidx emaxlen)
+                    (for/fold
+                      ([maxidx -1]
+                       [maxlen -1])
+                      ([idx (in-range 0 (sub1 (string-length s)))])
+                      (let ([newlen (expand-palindrome idx (add1 idx) s 0)])
+                        (if (< maxlen newlen)
+                            (values idx newlen)
+                            (values maxidx maxlen))))])
+        (if (>= omaxlen emaxlen)
+            (substring s (- omaxidx omaxlen) (+ omaxidx omaxlen 1))
+            (substring s (- (add1 emaxidx) emaxlen) (+ emaxidx emaxlen 1))))))
--- longest_palindrome/main_untyped_parallel.rkt
+++ longest_palindrome/main_untyped_parallel.rkt
@@ -1,30 +1,29 @@
-#lang typed/racket
-
-(: expand-palindrome (-> Integer Integer String Integer Integer))
+#lang racket
 (define (expand-palindrome l r s acc)
   (if (and (>= l 0) (< r (string-length s)) (eqv? (string-ref s l) (string-ref s r)))
       (expand-palindrome (sub1 l) (add1 r) s (+ acc 1))
       acc))
 
-(: longest-palindrome (-> String String))
 (define (longest-palindrome s)
   (if (<= (string-length s) 1)
       s
-      (let ([odd-comp ((inst future (Pair Integer Integer))
-                       (lambda () (let-values ([(x y) (for/fold
-      ([maxidx : Integer -1]
-       [maxlen : Integer -1])
-      ([idx (in-range 0 (string-length s))])
-                                                 (let ([newlen (expand-palindrome (sub1 idx) (add1 idx) s 0)])
-                                                   (if (< maxlen newlen)
-                                                       (values idx newlen)
-                                                       (values maxidx maxlen))))])
-                                    (cons x y)
-                                    )))])
+      (let ([odd-comp (future
+                       (lambda ()
+                         (let-values
+                             ([(x y)
+                               (for/fold
+                                   ([maxidx -1]
+                                    [maxlen -1])
+                                   ([idx (in-range 0 (string-length s))])
+                                 (let ([newlen (expand-palindrome (sub1 idx) (add1 idx) s 0)])
+                                   (if (< maxlen newlen)
+                                       (values idx newlen)
+                                       (values maxidx maxlen))))])
+                           (cons x y))))])
         (let-values ([(emaxidx emaxlen)
                       (for/fold
-                      ([maxidx : Integer -1]
-                       [maxlen : Integer -1])
+                      ([maxidx -1]
+                       [maxlen -1])
                       ([idx (in-range 0 (sub1 (string-length s)))])
                         (let ([newlen (expand-palindrome idx (add1 idx) s 0)])
                           (if (< maxlen newlen)
