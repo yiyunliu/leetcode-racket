81b79c7074cb9f860b527dca13c56f3e442ef905
Yiyun Liu <liuyiyun@seas.upenn.edu>
2025-09-29 00:55:44 -0400

    Add cleaner(?) but slightly slower binary tree level order algorithm

--- binary-tree-level-order/main.rkt
+++ binary-tree-level-order/main.rkt
@@ -9,30 +9,28 @@
 (define (make-tree-node [val 0])
   (tree-node val #f #f))
 
-(require racket/mutable-treelist)
-
 (define/contract (level-order root)
   (-> (or/c tree-node? #f) (listof (listof exact-integer?)))
-  (define visited (mutable-treelist))
   (define q (make-queue))
-  (enqueue! q (cons 0 root))
-  (define (loop)
+  (enqueue! q root)
+  (define (loop acc)
     (cond
-      [(queue-empty? q) (void)]
+      [(queue-empty? q) (reverse acc)]
       [else
-       (match-define (cons height root) (dequeue! q))
-       (cond
-         [(eqv? root 'push)]
-         [root
-          (define left (tree-node-left root))
-          (define right (tree-node-right root))
-          (define val (tree-node-val root))
-          (enqueue! q (cons (add1 height) left))
-          (enqueue! q (cons (add1 height) right))
-          (when (>= height (mutable-treelist-length visited))
-            (mutable-treelist-add! visited '()))
-          (mutable-treelist-set! visited height (cons val (mutable-treelist-ref visited height)))
-          (loop)]
-         [else (loop)])]))
-  (loop)
-  (map reverse (mutable-treelist->list visited)))
+       (define level-nodes
+         (for/fold ([acc '()])
+                   ([_ (in-range (queue-length q))])
+           (define root (dequeue! q))
+           (cond
+             [root
+              (define left (tree-node-left root))
+              (define right (tree-node-right root))
+              (define val (tree-node-val root))
+              (enqueue! q left)
+              (enqueue! q right)
+              (cons val acc)]
+             [else acc])))
+       (loop (cond
+               [(null? level-nodes) acc]
+               [else (cons (reverse level-nodes) acc)]))]))
+  (loop '()))
