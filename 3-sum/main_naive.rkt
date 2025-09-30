3974bb6f25550f9a0669a1e5d4f0c00f404c3a8f
Yiyun Liu <liuyiyun@seas.upenn.edu>
2025-09-30 19:32:59 -0400

    Add naive solution for 3-sum

---
+++ 3-sum/main_naive.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(define/contract (three-sum nums)
+  (-> (listof exact-integer?) (listof (listof exact-integer?)))
+  (define vnums (list->vector nums))
+  (vector-sort! vnums <)
+  (define len (vector-length vnums))
+  (define result
+    (for*/set
+        ([i (in-range (- len 2))]
+         [j (in-range (add1 i) (sub1 len))]
+         [k (in-range (add1 j) len )])
+      (list (vector-ref vnums i) (vector-ref vnums j) (vector-ref vnums k))))
+  (filter
+   (lambda (l)
+     (match-define (list i j k) l)
+     (zero? (+ i j k)))
+   (set->list result)))
