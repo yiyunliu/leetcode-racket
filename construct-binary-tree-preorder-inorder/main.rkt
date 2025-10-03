1e561fd371736e1701b947bc4182b6fd87f82103
Yiyun Liu <yliu8e5f7@protonmail.com>
2025-10-03 16:23:50 -0400

    Finish binary tree reconstruction

--- construct-binary-tree-preorder-inorder/main.rkt
+++ construct-binary-tree-preorder-inorder/main.rkt
@@ -6,24 +6,38 @@
 (define-struct
   tree-node ([val : Integer]
              [left : tree]
-             [right : tree]))
+             [right : tree])
+  #:transparent)
 
 (: build-tree (-> (Listof Integer) (Listof Integer) tree))
-(define (build-tree preorder inorder)
-  (define preorder-map
+(define (build-tree _preorder _inorder)
+  (define preorder #{(list->vector _preorder) : (Vectorof Integer)})
+  (define inorder #{(list->vector _inorder) : (Vectorof Integer)})
+  (define inorder-map
     (for/hash : (HashTable Integer Integer)
-        ([(a idx) (in-indexed preorder)])
+              ([(a idx) (in-indexed inorder)])
       (values a idx)))
-  (define (get-preorder i)
-    (hash-ref preorder-map i))
-  (let loop : (Values (Listof Integer)
-                      (U (Pair (U 'left 'right) tree) #f))
-       ([vals : (Listof Integer) inorder]
-        [parent-preorder : Integer ])
+  (: get-inorder (-> Integer Integer))
+  (define (get-inorder i)
+    (hash-ref inorder-map i))
+
+
+  (let loop : tree
+       ([pre-start : Integer 0]
+        [pre-end : Integer (vector-length preorder)]
+        [in-start : Integer 0]
+        [in-end : Integer (vector-length preorder)])
     (cond
-      [(null? vals) (values #f vals)]
+      [(zero? (- in-end in-start)) #f]
       [else
-       (match-define (cons val rest-vals) vals)
-       (define val-preorder (get-preorder val))
-       _]))
-  #f)
+       (define node (vector-ref preorder pre-start))
+       (define left-size (- (get-inorder node) in-start))
+       (define left-node (loop (add1 pre-start)
+                               (+ 1 left-size pre-start)
+                               in-start
+                               (+ in-start left-size)))
+       (define right-node (loop (+ pre-start 1 left-size)
+                                pre-end
+                                (+ in-start 1 left-size)
+                                in-end))
+       (tree-node node left-node right-node)])))
