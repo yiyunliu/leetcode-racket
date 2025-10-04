d572d56ceeb3e69323b488cdc861d96448defbeb
Yiyun Liu <yliu8e5f7@protonmail.com>
2025-10-04 16:51:39 -0400

    Replace ostringstream with string append

--- serialize-and-deserialize-bin-tree/main.cpp
+++ serialize-and-deserialize-bin-tree/main.cpp
@@ -3,47 +3,48 @@
 
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
+  int val;
+  TreeNode *left;
+  TreeNode *right;
 };
 
 class Codec {
 public:
 
-    // Encodes a tree to a single string.
-    string serialize(TreeNode* root) {
-	std::ostringstream s;
-	std::stack<TreeNode*> stack;
-	stack.push(root);
-	while (!stack.empty()) {
-	    auto node = stack.top();
-	    stack.pop();
-	    if (node) {
-		s << node -> val <<" ";
-		stack.push(node);
-	    }
-	    else {
-		s << 1001;
-	    }
-	}
-	return s.str();
+  // Encodes a tree to a single string.
+    std::string serialize(TreeNode* root) {
+    std::string s;
+    std::stack<TreeNode*> stack;
+    stack.push(root);
+    while (!stack.empty()) {
+      auto node = stack.top();
+      stack.pop();
+      if (node) {
+	s.append(std::to_string(node->val));
+	s.push_back(' ');
+	stack.push(node->right);
+	stack.push(node->left);
+      }
+      else {
+	s.append("1001 ");
+      }
     }
+    return s;
+  }
 
-    // Decodes your encoded data to tree.
-    TreeNode* deserialize(string data) {
-	std::istringstream s{data};
-	return deserialize(s);
-    }
+  // Decodes your encoded data to tree.
+  TreeNode* deserialize(std::string data) {
+    std::istringstream s{data};
+    return deserialize(s);
+  }
 
-    TreeNode* deserialize(std::istringstream &s) {
-	int i;
-	s >> i;
-	if (i == 1001) return nullptr;
+  TreeNode* deserialize(std::istringstream &s) {
+    int i;
+    s >> i;
+    if (i == 1001) return nullptr;
 
-	const auto left = deserialize(s);
-	const auto right = deserialize(s);
-	return new TreeNode {i, left, right};
-
-    }
+    const auto left = deserialize(s);
+    const auto right = deserialize(s);
+    return new TreeNode {i, left, right};
+  }
 };
