4fc6a789c6e2d0ab9dbe9103afbf5e25da875878
Yiyun Liu <liuyiyun@seas.upenn.edu>
2025-10-08 12:35:12 -0400

    Add a surprisingly slow solution for coin change

---
+++ coin-change/main_slow.rkt
@@ -0,0 +1,31 @@
+;; A top-down solution with memoization
+;; need to figure out why it times out on leetcode
+(module M typed/racket
+  (provide coin-change)
+  (: coin-change (-> (Listof Integer) Integer Integer))
+  (define (coin-change coins amount)
+    (: cache (Vectorof (U False 'Impossible Integer)))
+    (define cache (make-vector (+ amount 1) #f))
+    (vector-set! cache 0 0)
+    (for ([amount : Integer (in-range 1 (add1 amount))])
+      (let ([result
+             (for/fold ([acc : (U False Integer) #f])
+                       ([coin coins])
+               (let ([amount (- amount coin)])
+                 (if (negative? amount)
+                     acc
+                     (let ([cnt (vector-ref cache amount)])
+                       (cond
+                         [(false? cnt) (error "Impossible")]
+                         [(or (eqv? cnt 'Impossible) (negative? cnt)) acc]
+                         [(and acc cnt) (min acc (add1 cnt))]
+                         [cnt (add1 cnt)]
+                         [else acc])))))])
+        (vector-set! cache amount (or result 'Impossible))))
+    (let ([result (vector-ref cache amount)])
+      (case result
+        ['Impossible -1]
+        ['#f (error "impossible")]
+        [else result]))))
+
+(require 'M)
