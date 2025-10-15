84a785b593a33fccadad1a180b97a124382b35ad
Yiyun Liu <liuyiyun@seas.upenn.edu>
2025-10-15 16:35:02 -0400

    Add unit tests

--- number-of-islands/ocaml/main.ml
+++ number-of-islands/ocaml/main.ml
@@ -1,4 +1,5 @@
 open Batteries
+open OUnit2
 
 module type S = sig
   val num_islands : char array array -> int
@@ -31,6 +32,7 @@ module MatrixGraph : GRAPH with type t = char array array = struct
                     else None)), x + 1)
         else None)
     |> Enum.flatten
+    |> Enum.filter (fun (i,j) -> g.(i).(j) == '1')
   let neighbors (row,col) (g : t) =
     let num_rows = Array.length g
     and num_cols = Array.length (g.(0)) in
@@ -74,3 +76,42 @@ module Impl : S = struct
   open IslandComponents
   let num_islands = num_components
 end
+
+
+let grid1 =
+  [|[|'0';'1';'1';'1';'0'|]
+  ; [|'0';'1';'0';'1';'0'|]
+  ; [|'1';'1';'0';'0';'0'|]
+  ; [|'0';'0';'0';'0';'0'|]|]
+
+let grid2 =
+  [|[|'1';'1';'0';'0';'1'|]
+  ; [|'1';'1';'0';'0';'1'|]
+  ; [|'0';'0';'1';'0';'0'|]
+  ; [|'0';'0';'0';'1';'1'|]|]
+
+let grid3 =
+  [|[|'1';'1';'0';'0';'1'|]
+  ; [|'1';'1';'0';'0';'1'|]
+  ; [|'0';'0';'1';'0';'0'|]
+  ; [|'1';'1';'1';'1';'1'|]|]
+
+let grid4 =
+  [|[|'1';'1';'0';'0';'1'|]
+  ; [|'1';'1';'1';'0';'1'|]
+  ; [|'0';'0';'1';'0';'1'|]
+  ; [|'1';'1';'1';'1';'1'|]|]
+
+let test1 _ = assert_equal 1 (Impl.num_islands grid1)
+let test2 _ = assert_equal 4 (Impl.num_islands grid2)
+let test3 _ = assert_equal 3 (Impl.num_islands grid3)
+let test4 _ = assert_equal 1 (Impl.num_islands grid4)
+
+let suite =
+  "suite" >:::
+    ["test1" >:: test1
+    ;"test2" >:: test2
+    ;"test3" >:: test3
+    ;"test4" >:: test4]
+
+let _ = run_test_tt_main suite
