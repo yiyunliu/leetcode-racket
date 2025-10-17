ddc365fbe6095637100dd6985ab4edc9ce4b89e3
Yiyun Liu <yliu8e5f7@protonmail.com>
2025-10-16 22:53:23 -0400

    Add minstack solution with tests

--- minimum-stack/ocaml/main.ml
+++ minimum-stack/ocaml/main.ml
@@ -1,7 +1,7 @@
 open Batteries
+open OUnit2
 
 module type MINSTACK = sig
-  exception Empty_Stack
   type 'a t
   val push : 'a t -> 'a -> unit
   val create : unit -> 'a t
@@ -12,22 +12,51 @@ end
 
 module MinStack : MINSTACK = struct
   type 'a t = { stack : 'a Stack.t
-              ; minstack : 'a Stack.t
-              ; min : int}
-
-  let push {stack;min;ht} a =
-    let min = Int.min min a in
-    Stack.push a stack;
-    Hashtbl.modify_def 0 a (fun x -> x + 1) ht;
-    {stack;min;ht}
+              ; minstack : 'a Stack.t}
 
   let create () =
-    { stack = Stack.create ()
-    ; min = Int.max_num
-    ; ht = Hashtbl.create 10}
+    let stack = Stack.create () in
+    let minstack = Stack.create () in
+    { stack; minstack }
 
-  let pop {stack;min;ht} =
+  let push {stack;minstack} a =
+    Stack.push a stack;
+    if Stack.is_empty minstack || a <= Stack.top minstack
+    then Stack.push a minstack
+
+  let pop {stack;minstack} =
     let a = Stack.pop stack in
+    let min = Stack.top minstack in
+    if a = min then ignore (Stack.pop minstack)
 
+  let get_min {minstack;_} =
+    Stack.top minstack
 
+  let top {stack;_} = Stack.top stack
 end
+
+let test1 _ =
+  let ms = MinStack.create () in
+  MinStack.push ms 1;
+  MinStack.push ms 2;
+  MinStack.push ms 0;
+  assert_equal 0 (MinStack.get_min ms);
+  MinStack.pop ms;
+  assert_equal 2 (MinStack.top ms);
+  assert_equal 1 (MinStack.get_min ms)
+
+let test2 _ =
+  let ms = MinStack.create () in
+  MinStack.push ms (-2);
+  MinStack.push ms 0;
+  MinStack.push ms (-3);
+  assert_equal (-3) (MinStack.get_min ms);
+  MinStack.pop ms;
+  assert_equal 0 (MinStack.top ms);
+  assert_equal (-2) (MinStack.get_min ms)
+
+let suite =
+  "suite" >::: ["test1">::test1
+               ;"test2">::test2]
+
+let _ = run_test_tt_main suite
